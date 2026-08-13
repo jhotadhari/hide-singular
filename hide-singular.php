dc5b773b8cfba0361f3542bcd3ec2e412329a047
jhotadhari <tellme@waterproof-webdesign.de>
2026-08-13 21:04:30 +0200

    Build successful

--- hide-singular.php
+++ hide-singular.php
@@ -1,11 +1,11 @@
 <?php
 /*
-	Plugin Name: Hide Singular
-	Plugin URI: https://waterproof-webdesign.info/wppr_project/hide-singular
-	Description: Hide singular view for a specific postHide singular view for a specific post
+	Plugin Name: Croox Hide Singular
+	Plugin URI: https://github.com/jhotadhari/hide-singular
+	Description: Hide singular view for a specific post
 	Version: 0.0.0
 	Author: jhotadhari
-	Author URI: https://waterproof-webdesign.info
+	Author URI: https://github.com/jhotadhari
 	License: GNU General Public License v2 or later
 	License URI: http://www.gnu.org/licenses/gpl-2.0.html
 	Text Domain: hisi
